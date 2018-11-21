933f56f83bde6a49ec2f24cb94d8c1bf9d614679
Kirill Romanov <djaler1@gmail.com>
2018-11-21 12:24:46 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+        <v-content>
+            <v-container>
+                <v-list dense>
+                    <v-list-tile>
+                        <v-list-tile-content class="align-end">
+                            tile-content
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    }
+</script>
